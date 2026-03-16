949f52e9cb23ffd0b94932fb1265b24c37cecce8
Marcos Daniel da Silva Lima <marcos.d.s.lima1408@gmail.com>
2026-03-16 12:11:40 -0300

    feat: Adicionando comentário ao arquivo de teste para facilitar execução.

--- 4_Semestre/CI_CD/atividade_test/carrinho_test.py
+++ 4_Semestre/CI_CD/atividade_test/carrinho_test.py
@@ -1,5 +1,7 @@
 from carrinho import calcular_total, aplicar_desconto
 
+# Comando pra testar o projeto: pytest -v carrinho_test.py
+
 def test_calcular_total ():
     result = calcular_total(10,3)
     assert result == 30
